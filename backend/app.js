4278fa1492292ba6cd35df613dce45a27a461aab
Chike <kezielive@gmail.com>
2019-10-23 18:10:32 +0100

    final commit with all CRUD endpoints working

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+const Recipe = require('./models/recipe');
 
 mongoose.connect('mongodb+srv://test_case_user:DVnsmmwke2GdOzS5@cluster0-zub09.mongodb.net/test?retryWrites=true&w=majority')
   .then(() => {
@@ -24,14 +25,102 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use(bodyParser.json());
 
+  app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Thing created successfully!'
+  app.post('/api/recipes', (req, res, next) => {
+    const recipe = new Recipe({
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      difficulty: req.body.difficulty,
+      time: req.body.time,
+      
     });
+    recipe.save().then(
+      () => {
+        res.status(201).json({
+          message: 'Post saved successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+  app.get('/api/recipes/:id', (req, res, next) => {
+    Recipe.findOne({
+      _id: req.params.id
+    }).then(
+      (recipe) => {
+        res.status(200).json(recipe);
+      }
+    ).catch(
+      (error) => {
+        res.status(404).json({
+          error: error
+        });
+      }
+    );
+  });
+
+  app.put('/api/recipes/:id', (req, res, next) => {
+    const recipe = new Recipe({
+      _id: req.params.id,
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      difficulty: req.body.difficulty,
+      time: req.body.time,
+    });
+    Recipe.updateOne({_id: req.params.id}, recipe).then(
+      () => {
+        res.status(201).json({
+          message: 'Thing updated successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+
+  app.delete('/api/recipes/:id', (req, res, next) => {
+    Recipe.deleteOne({_id: req.params.id}).then(
+      () => {
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+  app.get('/api/recipes', (req, res, next) => {
+    Recipe.find().then(
+      (recipes) => {
+        res.status(200).json(recipes);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
   });
 
 
